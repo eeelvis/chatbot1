81712c17780c7b0ecb3c1ad0c4b90ceeb16441f0
eeelvis <elvis.mcp@gmail.com>
2016-11-24 13:48:48 -0200

    Testes

--- app.js
+++ app.js
@@ -14,39 +14,15 @@ var connector = new builder.ChatConnector
 ({ appId: '471f5249-54e9-4f28-b9c8-a84bce3d8b73', appPassword: 'jLvpTor0mwSdigQXhJvZmAr' }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
-/*
-// Create bot dialogs
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
-*/
-
-// Variaveis do LUIS
-
-var model = 'https://api.projectoxford.ai/luis/v2.0/apps/987147e7-41a0-4e0f-b290-f1846d2bf64c?subscription-key=a152b2b2c13b46518fceb82ca064b920&q=ol%C3%A1&verbose=true';
-var recognizer = new builder.LuisRecognizer(model);
-var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
-
-bot.dialog('/', dialog); 
-
-//dialog.matches('Saudacao', builder.DialogAction.send('Entendi que voce quer dizer ola'));
-
-dialog.matches('Saudacao', [
-
-    function (session, args, next) {
-
-		session.beginDialog('/askName');
-
-		//session.beginDialog('/ensureProfile', session.userData.profile);
-
+// Após o primeiro input do usuário, será exibida a mensagem 'Olá! Me diga o seu nome.'
+bot.dialog('/', [
+    function(session) {
+        builder.Prompts.text(session, 'Olá! Me diga o seu nome.');
     },
-
-    function (session, results) {
-
-        session.send('Ola %s!', results.response);
-
-        session.send('Como posso te ajudar?');
-
+    // Após responder com o nome, a mensagem 'Seja bem vindo Nome' será exibida.
+    function(session, results) {
+        session.send('Seja bem vindo %s!', results.response);
     }
 
 ]);
+
